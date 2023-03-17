5b103c70d784fd9e159cf383fa8d6ecb05bdf351
Mosihere <Mostafakhoshhal@yahoo.com>
2023-03-17 19:30:43 +0330

    test-cases updated for searching movies.

--- test.py
+++ test.py
@@ -1,12 +1,11 @@
 import unittest
-from api import get_top_250_movies, get_top_250_series
+from api import get_top_250_movies, get_top_250_series, search_movie
 
 
 API_KEY = input('insert your api_key:\n')
 
 class TestMovies(unittest.TestCase):
     def test_top_movies(self):
-
         '''
         Check response type is list or not.
         '''
@@ -15,10 +14,17 @@ class TestMovies(unittest.TestCase):
         self.assertIsInstance(result, list)
 
     def test_top_series(self):
-
         '''
         Check response type is list or not.
         '''
 
         result = get_top_250_series(API_KEY)
+        self.assertIsInstance(result, list)
+
+    def test_search_movie(self):
+        '''
+        Check response type is list or not.
+        '''
+
+        result = search_movie(API_KEY, 'inception')
         self.assertIsInstance(result, list)
